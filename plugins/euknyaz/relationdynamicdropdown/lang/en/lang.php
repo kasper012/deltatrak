f97d5d8fc256d3d901e0161dc76a62c1393cf748
kasper012 <adyl.syubaev@gmail.com>
2021-12-22 15:14:43 +0400

    fixes

--- plugins/euknyaz/relationdynamicdropdown/lang/en/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'plugin' => [
-        'name' => 'Relation Dynamic Dropdown Form Widget',
-        'description' => 'Extends back-end forms with new field type: relation-dynamic-dropdown',
-        'author' => 'Eugene Knyazkov'
-    ]
-];

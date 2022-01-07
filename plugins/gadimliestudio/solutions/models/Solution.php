6931580291f52ec83060129a2dfef70c9285f3ae
kasper012 <adyl.syubaev@gmail.com>
2022-01-07 11:22:28 +0400

    translate-fixes

--- plugins/gadimliestudio/solutions/models/Solution.php
+++ plugins/gadimliestudio/solutions/models/Solution.php
@@ -9,7 +9,20 @@ class Solution extends Model
 {
     use \October\Rain\Database\Traits\Validation;
 
+    public $implement = [
+        'RainLab.Translate.Behaviors.TranslatableModel'
+      ];
+    
+    public $translatable = [
+        'title',
+        'banner_text',
+        'problem_text',
+        'currently_text',
+        'product_about_text',
+        'selected',
+      
 
+    ];
 
     /**
      * @var string The database table used by the model.

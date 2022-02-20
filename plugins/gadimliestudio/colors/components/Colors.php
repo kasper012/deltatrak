6f7c59b9223cdc6e1088b027057701cf16c117f3
kasper012 <adyl.syubaev@gmail.com>
2022-02-20 23:32:09 +0400

    colors

--- plugins/gadimliestudio/colors/components/Colors.php
+++ plugins/gadimliestudio/colors/components/Colors.php
@@ -28,14 +28,43 @@ class Colors extends ComponentBase
 
 
         $colors->top_navbar = $settings->top_navbar;
-
-
-        $colors->footer = $settings->footer;
-        
-        $colors->footer_second_color = $settings->footer_second_color;
-        $colors->footer_second_color = $settings->footer_second_color;
-        
         $colors->is_footer_gradient = $settings->is_footer_gradient;
+        $colors->footer = $settings->footer;
+        $colors->footer_second_color = $settings->footer_second_color;
+        $colors->products_list_background = $settings->products_list_background;
+
+        
+
+        
+        $colors->title_on_colorful_background = $settings->title_on_colorful_background;
+        $colors->main_text_color = $settings->main_text_color;
+        $colors->links_color = $settings->links_color;
+        $colors->footer_links_hover = $settings->footer_links_hover;
+        $colors->button_background_color = $settings->button_background_color;
+        $colors->button_color = $settings->button_color;
+        $colors->icons_color = $settings->icons_color;
+
+
+
+
+
+        $colors->products_bg = $settings->products_bg;
+        $colors->support_bg = $settings->support_bg;
+        $colors->warranty_bg = $settings->warranty_bg;
+        $colors->software_bg = $settings->software_bg;
+        $colors->validation = $settings->validation;
+        $colors->calibration = $settings->calibration;
+        $colors->calibration_17025 = $settings->calibration_17025;
+        $colors->gogreen_bg = $settings->gogreen_bg;
+        $colors->about_bg = $settings->about_bg;
+        $colors->contact_bg = $settings->contact_bg;
+
+
+        
+        
+        
+
+        
 
 
         $this->page['colors'] = $colors;
